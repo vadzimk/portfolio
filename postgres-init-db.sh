4225ff85ad3b0ab85ba53969449b69c75617cfad
Vadzim <vadzimk@hotmail.com>
2022-11-26 19:04:29 -0800

    Update .gitlab-ci.yml, init db if not exist

---
+++ postgres-init-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if psql -U $POSTGRES_USER -lqt | cut -d \| -f 1 | grep -qw $DATABASE_NAME);
+then
+  echo "$DATABASE_NAME exists"
+else
+  createdb -U $POSTGRES_USER -T template0 $DATABASE_NAME && echo "$DATABASE_NAME created"
+fi
+
+# this step is done manually using ssh from local machine.
+# scp * ubuntu@vadzimk.link:~/applications/common_db/backup
+# docker exec -it postgres bash "pg_restore -U postgres --dbname=$DATABASE_NAME --verbose --no-owner ./backup/dump.tar"
