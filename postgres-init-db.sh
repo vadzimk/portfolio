afbff40fce41e929dfcf76c2e49b08e69bd76b9f
Vadzim <vadzimk@hotmail.com>
2022-11-27 17:58:07 -0800

    make init db a separate stage

--- postgres-init-db.sh
+++ postgres-init-db.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 set -e
+echo POSTGRES_USER=$POSTGRES_USER;
+echo DATABASE_NAME=$DATABASE_NAME;
 
 if psql -U $POSTGRES_USER -lqt | cut -d \| -f 1 | grep -qw $DATABASE_NAME;
 then
-  echo "$DATABASE_NAME exists"
+  echo $DATABASE_NAME exists;
 else
-  createdb -U $POSTGRES_USER -T template0 $DATABASE_NAME && echo "$DATABASE_NAME created"
-fi
+  createdb -U $POSTGRES_USER -T template0 $DATABASE_NAME && echo $DATABASE_NAME created;
+fi;
 
 # this step is done manually using ssh from local machine.
 # scp * ubuntu@vadzimk.link:~/applications/common_db/backup
