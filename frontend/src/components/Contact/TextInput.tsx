73d55814bd99eb865bd5543d3d678d5cbddc3118
Vadzim <vadzimk@hotmail.com>
2022-12-10 23:14:17 -0800

    label font increase

--- frontend/src/components/Contact/TextInput.tsx
+++ frontend/src/components/Contact/TextInput.tsx
@@ -35,7 +35,7 @@ export default function TextInput ({type, label, name, options, className}: Inpu
                     `}
                 />
                 <label htmlFor={`input_${name}`}
-                       className="absolute text-xs duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 opacity-70">
+                       className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 opacity-70">
                     {label}
                 </label>
             </div>
