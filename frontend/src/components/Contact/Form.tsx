f47f16b69b7c0e4dfc433bfc9a3ad91876043152
Vadzim <vadzimk@hotmail.com>
2022-12-10 23:11:42 -0800

    add form validation, correct input id misnamed

--- frontend/src/components/Contact/Form.tsx
+++ frontend/src/components/Contact/Form.tsx
@@ -1,6 +1,7 @@
 import {FormProvider, useForm} from 'react-hook-form';
 import React from 'react';
 import TextInput from '@/components/Contact/TextInput';
+
 export interface IFormValues {
     "message": string;
     "subject": string;
@@ -8,12 +9,12 @@ export interface IFormValues {
 }
 
 const Arrow = <svg width="14" height="22" fill="none" className="text-secondary1 fill-current scale-[.80]">
-    <path  d="M9.37 10.84.333 19.879l1.917 1.918L13.296 10.75 2.75.204.742 2.212l8.628 8.629Z"/>
+    <path d="M9.37 10.84.333 19.879l1.917 1.918L13.296 10.75 2.75.204.742 2.212l8.628 8.629Z"/>
 </svg>
 
-export default function Form ({onSubmit}){
+export default function Form({onSubmit}) {
     const methods = useForm<IFormValues>();
-
+    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
     return (
         <FormProvider {...methods}>
             <form className="flex flex-col gap-8 sm:flex-row"
@@ -23,17 +24,25 @@ export default function Form ({onSubmit}){
                     className="h-72 grow"
                     label="Message"
                     name="message"
-                    required/>
+                    options={{required: "Required"}}
+                />
                 <div className="flex flex-col gap-8 grow sm:justify-between">
                     <div className="flex flex-col gap-8">
                         <TextInput
                             label="Subject"
                             name="subject"
-                            required/>
+                            options={{required: "Required"}}
+                        />
                         <TextInput
                             label="Your email"
                             name="email"
-                            required/>
+                            options={{
+                                required: "Required",
+                                pattern: {
+                                    value: emailRegex,
+                                    message: "Invalid email"
+                                }
+                            }}/>
                     </div>
                     <button type="submit"
                             className="w-full font-serif text-xl text-center rounded px-5 py-2.5
--- frontend/src/components/Contact/TextInput.tsx
+++ frontend/src/components/Contact/TextInput.tsx
@@ -1,4 +1,4 @@
-import {Path, useFormContext} from 'react-hook-form';
+import {Path, RegisterOptions, useFormContext} from 'react-hook-form';
 import React from 'react';
 import {IFormValues} from './Form';
 
@@ -6,24 +6,24 @@ type InputProps = {
     type?: string;
     label: string;
     name: Path<IFormValues>;
-    required: boolean;
+    options: RegisterOptions;
     className?: string;
 };
 
-export default function TextInput ({type, label, name, required, className}: InputProps) {
+export default function TextInput ({type, label, name, options, className}: InputProps) {
     const {register, formState: {errors}} = useFormContext(); // retrieve all hook methods
     console.log(name, errors[name])
 
-    let Component = (props) => <input {...props} {...register(name, {required: "Required"})}/> as React.ReactElement
+    let Component = (props) => <input {...props} {...register(name, options)}/> as React.ReactElement
     if (type === 'textarea')
-        Component = (props) => <textarea {...props} {...register(name, {required: "Required"})}/> as React.ReactElement
+        Component = (props) => <textarea {...props} {...register(name, options)}/> as React.ReactElement
 
     return (
         <div className={className}>
             <div className="relative h-full">
                 <Component
                     type="text"
-                    id="filled_error"
+                    id={`input_${name}`}
                     aria-describedby="filled_error_help"
                     placeholder=" "
                     className={`block px-3 pb-2.5 pt-5 w-full h-full bg-white
@@ -34,8 +34,8 @@ export default function TextInput ({type, label, name, required, className}: Inp
                     overflow-scroll resize-none
                     `}
                 />
-                <label htmlFor="filled_error"
-                       className="absolute text-sm duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 opacity-70">
+                <label htmlFor={`input_${name}`}
+                       className="absolute text-xs duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-3 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 opacity-70">
                     {label}
                 </label>
             </div>
