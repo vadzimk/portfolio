0977509819f9f41eb2322d0871f91c42710470b3
Vadzim <vadzimk@hotmail.com>
2023-02-04 11:24:08 -0800

    fix: Warning: Prop `d` did not match. Server

---
+++ frontend/src/hooks/useLoaded.tsx
@@ -0,0 +1,9 @@
+import {useEffect, useState} from "react";
+
+export default function useLoaded () {
+    const [loaded, setLoaded] = useState(false);
+    useEffect(() =>
+        setLoaded(true), []
+    );
+    return loaded;
+};
